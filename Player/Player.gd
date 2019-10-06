99b24fc04d4e9fb4cdc08bf1a9b8a08b58f581e7
adammcclenaghan <adam@mcclenaghan.co.uk>
2019-10-06 13:50:20 +0100

    Hazards, sounds and game over

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,6 +6,9 @@ const GRAVITY = 150
 const UP = Vector2(0,-1)
 const DOWN = Vector2(0,1)
 const JUMP_SPEED = 3500
+const WORLD_LIMIT = 4000
+
+var lives = 3
 
 signal animate
 
@@ -28,9 +31,12 @@ func move():
 func jump():
 	if Input.is_action_pressed("jump") and is_on_floor():
 		motion.y -= JUMP_SPEED
-
+		$AudioStreamPlayer.stream = load("res://SFX/jump1.ogg")
+		$AudioStreamPlayer.play()
 
 func apply_gravity():
+	if position.y > WORLD_LIMIT:
+		end_game()
 	if is_on_floor():
 		motion.y = 0
 	elif is_on_ceiling():
@@ -41,3 +47,16 @@ func apply_gravity():
 
 func animate():
 	emit_signal("animate", motion)
+	
+func end_game():
+	get_tree().change_scene("res://Levels/GameOver.tscn")
+	
+func hurt():
+	position.y -= 1 # Set player to not be n ground to not compete with is_on_floor()
+	yield(get_tree(), "idle_frame") # Wait for one frame
+	motion.y -= JUMP_SPEED # Visual cue for player
+	lives -= 1
+	$AudioStreamPlayer.stream = load("res://SFX/pain.ogg")
+	$AudioStreamPlayer.play()
+	if lives <= 0:
+		end_game()
